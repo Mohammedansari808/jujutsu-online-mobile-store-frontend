12926a9862c4a4424f9e724732096ec20d48b681
Mohammedansari808 <mohammedansari808@gmail.com>
2023-01-08 12:50:47 +0530

    task uploaded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Products />
     </div>
   );
 }
 
+
+
+
+
+function Products() {
+  const [mobiles, setMobiles] = useState([])
+
+  const mobile = () => fetch("https://tired-gold-cape.cyclic.app/", {
+    method: 'GET',
+  })
+    .then(data => data.json())
+    .then(res => setMobiles(res))
+  mobile()
+  return (
+
+    <div className='phone-list-container'>
+      {
+        mobiles.map((mobile) => (<Mobiles mobile={mobile} />))
+      }
+    </div>
+  )
+}
+
+
+function Mobiles({ mobile }) {
+  return (
+    <div className='phone-container'>
+      <img alt={mobile.model} className='phone-picture' src={mobile.img} />
+      <h2 className='phone-name'>{mobile.model}</h2>
+      <h3 className='phone-company'>{mobile.company}</h3>
+    </div>
+  )
+}
 export default App;
